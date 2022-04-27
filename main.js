fc002f9c7c9767428535d8bde489b1931978a426
Thabiso Kubeka <65237633+Thabiso3028@users.noreply.github.com>
2022-04-27 08:39:17 +0200

    buttons

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function UserInput(){
+    var out = document.getElementById("inputDisp");
+
+    out.setAttribute("value", "Thabiso");
+}
+
+function add(fstInput, secInput){
+    return fstInput + secInput;
+}
